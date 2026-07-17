7c69f43a5b15cdeadc2a60df9b29a9517380c473
Kunj Sonker <184824849+kunjsonker@users.noreply.github.com>
2026-07-17 18:02:03 +0530

    Time: 3 ms (68.16%), Space: 10.9 MB (41.68%) - LeetHub

---
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    int characterReplacement(string s, int k) {
+
+        //sliding window
+        //we will use hashmap in this question with dynamic sliding window
+
+        unordered_map<char,int>mpp;
+
+        int l=0;
+        int maxx=0;int ans=0;
+
+        for(int r=0;r<s.size();r++){
+            
+            mpp[s[r]]++;
+            maxx=max(maxx,mpp[s[r]]);
+
+            while((r-l+1)-maxx>k){
+                mpp[s[l]]--;
+                l++;
+                
+            }
+            
+            ans=max(maxx,r-l+1);
+        }
+        return ans;
+
+
+
+        
+
+
+
+
+
+
+        
+    }
+};
