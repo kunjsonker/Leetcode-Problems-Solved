36ac270a8aa579ad830575275893c8ce32e0689f
Kunj Sonker <184824849+kunjsonker@users.noreply.github.com>
2026-07-24 23:33:56 +0530

    Time: 0 ms (100%), Space: 7.8 MB (44.64%) - LeetHub

--- 0263-ugly-number/0263-ugly-number.cpp
+++ 0263-ugly-number/0263-ugly-number.cpp
@@ -1,6 +1,8 @@
 class Solution {
 public:
     bool isUgly(int n) {
+        //very easy math problem divide it by 5 3 2 while it can be divisible
+        //check if the number remaining is 1 this means that it is divisible by 5 3 2 
 
         if(n==0) return false;
 
